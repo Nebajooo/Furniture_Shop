39c63ccdbdfdcb86f6aae842d219c755ca2a5dcd
Nebajooo <nebajoo2015@gmail.com>
2024-03-11 01:30:13 -0700

    update in contentful.js code

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,8 @@
 import { createClient } from "contentful";
 
-export default createClient({
+const client = createClient({
   space: process.env.REACT_APP_API_SPACE,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
+
+export default client;
