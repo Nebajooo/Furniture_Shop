190bd453d2f082d5558fac56283901275eaf1df8
Nebajooo <nebajoo2015@gmail.com>
2024-03-11 00:21:18 -0700

    upload new project code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import SingleProduct from "./pages/SingleProduct";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
+import { Route, Switch } from "react-router-dom";
 
-function App() {
+// const url = (address) => {
+//   window.location.href = address;
+// }
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/products/:slug" exact component={SingleProduct} />
+        <Route path="/contact" exact component={Contact} />
+        <Route path="/gallery" exact component={Gallery} />
+        <Route
+          path="/joinwhatsapp"
+          component={() => {
+            window.location.href = "https://chat.whatsapp.com/";
+          }}
+        />
+        <Route
+          path="/joininsta"
+          component={() => {
+            window.location.href = "https://www.instagram.com/";
+          }}
+        />
+        <Route
+          path="/joinfacebook"
+          component={() => {
+            window.location.href = "https://www.facebook.com/";
+          }}
+        />
+        <Route
+          path="/locate"
+          component={() => {
+            window.location.href = "https://g.page/furniture?share";
+          }}
+        />
+        <Route component={Error} />
+      </Router>{" "}
+    </>
   );
-}
+};
 
 export default App;
